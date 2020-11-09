65822320f559e1f7aeb59e571957cc21d2975189
eShopSolution <hokien1992@gmail.com>
2020-11-09 10:14:22 +0700

    Design Database

---
+++ eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+	// Config theo dạng Attribute
+	//[Table("Products")]
+	public class Product
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+		public decimal OriginalPrice { get; set; }
+		public int Stock { get; set; }
+		public int ViewCount { get; set; }
+		// Config theo dạng Attribute
+		//[Required]
+		public DateTime DateCreated { get; set; }
+		public string SeoAlias { get; set; }
+	}
+}
