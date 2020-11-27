be00100e9dfd208f926c6b6d54982bc5f3cb7fec
eShopSolution <hokien1992@gmail.com>
2020-11-26 16:19:40 -0800

    sd

---
+++ eShopSolution.Data/Migrations/20201127001405_AspNetCoreIdentityDatabase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class AspNetCoreIdentityDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2020, 11, 26, 16, 14, 4, 488, DateTimeKind.Local).AddTicks(1849),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 11, 26, 16, 9, 16, 956, DateTimeKind.Local).AddTicks(8948));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 11, 26, 16, 14, 4, 509, DateTimeKind.Local).AddTicks(3643));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 11, 26, 16, 9, 16, 956, DateTimeKind.Local).AddTicks(8948),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 11, 26, 16, 14, 4, 488, DateTimeKind.Local).AddTicks(1849));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Status",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DateCreated",
+                value: new DateTime(2020, 11, 26, 16, 9, 16, 979, DateTimeKind.Local).AddTicks(4666));
+        }
+    }
+}
